4dff432f7f034fba770a97ca4e9cdceee4fa0e6c
mattperkins <github@mattperkins.com>
2018-08-22 18:53:43 +0100

    add css reset and modest grid

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
+import './css/reset.css'
 import './css/style.css'
+import './css/form.css'
 
 // main ("ROOT") component 
 class Root extends React.Component {
@@ -10,9 +12,22 @@ render() {
 
 return (
 
-<div>
-<h1>Hello, World!</h1>
+
+<div className="wrapper">
+<div className="row">
+
+<div className="col-6">
+  <h1>Hello, World!</h1>
 </div>
+
+<div className="col-6">
+  <input type="text" placeholder="Put your text here"/>
+  <button className="button-primary">I am a button</button>
+</div>
+
+</div>
+</div>
+
 
 )// end return
 }// end render
